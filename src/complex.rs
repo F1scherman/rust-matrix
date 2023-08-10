25aaa00d11a0f4c91d979d2fc094a212e7aee168
Brayden <brayden.f.jonsson@gmail.com>
2023-08-10 10:24:33 -0600

    Added basic operators

--- src/complex.rs
+++ src/complex.rs
@@ -3,6 +3,9 @@
 ///
 /// Contains a struct and methods for representing a complex number
 
+#[macro_use] extern crate gen_ops;
+use std::ops;
+
 trait_set! {
     pub trait ComplexCompatible = num_traits::NumAssign
     + num_traits::sign::Signed
@@ -19,17 +22,76 @@ where
     imaginary: T,
 }
 
-impl<T> NumAssign for ComplexNumber<T> where T: ComplexCompatible {
-    fn add(self, rhs: Self) -> Self {
+impl<T> ComplexNumber<T>
+where T: ComplexCompatible, {
+
+    pub fn conjugate (&self) -> Self {
         Self {
-            real + rhs.real,
-            imaginary + rhs.imaginary
-        }
-    }
-    fn sub(self, rhs: Self) -> Self {
-        Self {
-            real - rhs.real,
-            imaginary - rhs.imaginary
+            real,
+            imaginary.neg(),
         }
     }
 }
+
+gen_ops!(
+    <T>;
+    types ComplexNumber<T>, ComplexNumber<T> => ComplexNumber<T>;
+
+    /// Add two complex numbers
+    /// 
+    /// Returns a complex number
+    for + call |a: &ComplexNumber<T>, b: &ComplexNumber<T>| ComplexNumber(a.real + b.real, a.imaginary + b.imaginary);
+
+    /// Subtract two complex numbers
+    /// 
+    /// Returns a complex number
+    for - call |a: &ComplexNumber<T>, b: &ComplexNumber<T>| ComplexNumber(a.real - b.real, a.imaginary - b.imaginary);
+
+    /// Multiply two complex numbers
+    /// 
+    /// Returns a complex number
+    for * call |a: &ComplexNumber<T>, b: &ComplexNumber<T>| ComplexNumber(a.real * b.real - a.imaginary * b.imaginary, a.real * b.imaginary + a.imaginary * b.real);
+
+    /// Divide two complex numbers
+    /// 
+    /// Returns a complex number
+    for / call |a: &ComplexNumber<T>, b: &ComplexNumber<T>| (a * b.conjugate()) / (b.real * b.real + b.imaginary * b.imaginary);
+
+    where T: ComplexCompatible
+);
+
+gen_ops_ex!(
+    <T>;
+    types ComplexNumber<T>, T => ComplexNumber<T>;
+
+    /// Scales a complex number
+    /// 
+    /// Returns a complex number
+    for / call |vector: &ComplexNumber<T>, scalar:&T| ComplexNumber(vector.real / *scalar, vector.imaginary / *scalar);
+
+    where T: ComplexCompatible
+);
+
+gen_ops_ex!(
+    <T>;
+    types T, ComplexNumber<T> => ComplexNumber<T>;
+
+    /// Divides the given real number by a complex number
+    /// 
+    /// Returns a complex number
+    for / call |real:&T, complex:&ComplexNumber| (complex.conjugate() * real) / (complex.real * complex.real + complex.imaginary * complex.imaginary);
+
+    where T: ComplexCompatible
+);
+
+gen_ops_comm_ex!(
+    <T>;
+    types ComplexNumber<T>, T => ComplexNumber<T>;
+
+    /// Scales a complex number
+    /// 
+    /// Returns a complex number
+    for * call |vector: &ComplexNumber<T>, scalar:&T| ComplexNumber(vector.real * *scalar, vector.imaginary * *scalar);
+
+    where T: ComplexCompatible
+);
